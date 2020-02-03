ea9a5bed738045d62e81cf733a853d5ce7baff08
yopyzzang <yopyzzang@gmail.com>
2020-02-04 03:04:59 +0900

    teamproject

---
+++ src/main/java/com/yopy/mvcboard/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.yopy.mvcboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("greeting", "hello world");
+        return "home";
+    }
+
+}
